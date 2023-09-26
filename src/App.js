aff28b917df62bf30cf2923b39c437d331189fc7
Shruti Pal <shrutipal700@gmail.com>
2023-09-26 18:50:57 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,74 @@
-import Login from './components/Login';
-import Dashboard from './components/Dashboard';
-import { useAuth0 } from '@auth0/auth0-react';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const { isLoading, error } = useAuth0()
+  const menuItems = [
+    {
+      name: 'Dish 1',
+      description: 'Pasta bolognesa',
+    },
+    {
+      name: 'Dish 2',
+      price: '$12.99',
+    },
+    {
+      name: 'Soups',
+      description: 'Soup.',
+      price: '$18.99',
+    },
+  ]
 
-  return (
-    <main className="column">
-      {error && <p>Authentication Error</p>}
-      {!error && isLoading && <p>Loading...</p>}
-      {!error && !isLoading && (
-        <>
-          <Login/>
-          <Dashboard />
-        </>
-      )}
-    </main>
-  )
+   return (
+     <div>
+       <header>
+         <h1>Welcome to Tortilla Restaurant</h1>
+         
+       </header>
+
+       <nav>
+         <ul>
+           <li>
+             <a href="#">Home</a>
+           </li>
+           <li>
+             <a href="#">Menu</a>
+           </li>
+           <li>
+             <a href="#">Contact</a>
+           </li>
+         </ul>
+       </nav>
+
+       <main>
+         <section>
+           <h2>Our Menu</h2>
+           <p>Explore our delicious menu offerings.</p>
+           <ul>
+             {menuItems.map((item, index) => (
+               <li key={index}>
+                 <h3>{item.name}</h3>
+                 <p>{item.description}</p>
+                 <p>
+                   <strong>Price:</strong> {item.price}
+                 </p>
+               </li>
+             ))}
+           </ul>
+         </section>
+
+         <section>
+          
+           <p>Learn more about our restaurant and story.</p>
+         </section>
+       </main>
+
+       <footer>
+         <p>&copy; 2023 Tortilla Restaurant. All rights reserved.</p>
+       </footer>
+     </div>
+   )
 }
 
 export default App
